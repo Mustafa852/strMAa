69d7987b9ce07cf4cd2021ed5889d3e77f79c604
Glitch (codework-botu) <none>
2020-07-11 12:14:09 +0000

    🐮👡 Checkpoint ./komutlar/yardım.js:11195635/117

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,11 +5,11 @@ const mhelp = new Discord.RichEmbed()
 .setColor("#5ab1bb")
 .setTitle("Upper Yardım Menüsü")
 .setThumbnail("https://cdn.discordapp.com/attachments/718563633009000531/731477372674768916/unnamed.png")
-.addField("<a:ayars:731479176904704060> /moderasyon", "Moderasyon Komutlarını Listeler!")
-.addField("<a:ayars:731479176904704060> /koruma","Koruma Komutlarını Listeler!")
-.addField("<a:ayars:731479176904704060> /kullanıcı", "Kullanıcı Komutlarını Listeler!")
-.addField(`<a:zilbe:731478436731682816> Bot Hakkında`,`
-      [Davet Et](Yakında) | [Oyver](Yakında) | [Support Server](https://discord.gg/Z2zuGCR) `)
+.addField("<a:ayars:731479176904704060> /moderasyon", "» Moderasyon Komutlarını Listeler!")
+.addField("<a:ayars:731479176904704060> /koruma","» Koruma Komutlarını Listeler!")
+.addField("<a:ayars:731479176904704060> /kullanıcı", `» Kullanıcı Komutlarını Listeler!
+
+   <a:zilbe:731478436731682816> » [Davet Et](Yakında) | [Oyver](Yakında) | [Support Server](https://discord.gg/Z2zuGCR) `)
 message.channel.sendEmbed(mhelp)
 
 }; 
