16273bb428b86c09c04da21e3c46367ad1938603
Glitch (codework-botu) <none>
2020-07-11 18:14:21 +0000

    🌹🙅 Checkpoint ./komutlar/uyar.js:11195635/99 ./komutlar/yardım-mod.js:11195635/958

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -11,8 +11,8 @@ exports.run = (client, message, args) => {
   
   let user = message.mentions.users.first();
   let reason = args.slice(1).join(' ');
-  if (db.has(`log_${message.guild.id}`) === false) return message.reply('Mod log kanalı ayarlanmamış');
-  let modlog = message.guild.channels.get(db.fetch(`log_${message.guild.id}`).replace("<#", "").replace(">", ""));
+  if (db.has(`codeminglog_${message.guild.id}`) === false) return message.reply('Mod log kanalı ayarlanmamış');
+  let modlog = message.guild.channels.get(db.fetch(`codeminglog_${message.guild.id}`).replace("<#", "").replace(">", ""));
   if (message.mentions.users.size < 1) return message.reply('Uyaracağın kişiyi etiketlemelisin!');
   if (reason.length < 1) return message.reply('Uyarma sebebini yazmadın!');
   if (user.id === message.author.id) return message.reply('Kendini uyaramazsın!');
@@ -24,7 +24,7 @@ exports.run = (client, message, args) => {
   .addField('Yapılan İşlem', 'Uyarma')
   .addField('Uyarılan Kullanıcı', `${user.tag} (${user.id})`)
   .addField('Uyaran Yetkili', `${message.author.username}#${message.author.discriminator}`)
-  .addField('Uyarı Sebebi', "```" + reason + "```")
+  .addField('Uyarı Sebebi', `${reason}`)
   modlog.send(embed);
   
   message.guild.members.get(user.id).send(`<@${user.id}>, \n**${message.guild.name}** adlı sunucuda **${reason}** sebebi ile uyarıldın! \nKuralları çiğnemeye devam eder isen susturulabilir, atılabilir veya yasaklanabilirsin!`)
--- komutlar/yardım-mod.js
+++ komutlar/yardım-mod.js
@@ -10,15 +10,14 @@ const mhelp = new Discord.RichEmbed()
 .addField("<a:ayars:731479176904704060> sil-üye <@kullanıcı> <1-100 Arası Sayı>", "» İstediğiniz Kullanıcının Mesajını Siler!")
 .addField("<a:ayars:731479176904704060> modlog <#kanal>" , "» Mod Log Kanalını Ayarlarsınız!")
 .addField("<a:ayars:731479176904704060> kick <@kullanıcı> <sebep>", "» Kullanıcıyı Sunucudan Atar!")
-.addField("<a:ayars:731479176904704060> mute <@kullanıcı> <sebep>", "» Kullanıcıyı Susturur!")
-.addField("<a:ayars:731479176904704060> sürelimute <@kullanıcı> <süre> <sebep>" , "» Kullanıcıyı Süreli Susturur!")
 .addField("<a:ayars:731479176904704060> sunucudetay", "» Sunucu Hakkında Detayları Gösterir!")
 .addField("<a:ayars:731479176904704060> yavaşmod <0-180 Arası Süre>", "» Yavaş Modu Açar!")
 .addField("<a:ayars:731479176904704060> sa-as <aç-kapat>", "» Sa-As Sistemini Aktif Eder!")
-.addField("<a:ayars:731479176904704060> unmute <member>" , "» Unmutes a member")
 .addField("<a:ayars:731479176904704060> süreliban <@kullanıcı> <süre>" , "» Unmutes a member")
 .addField("<a:ayars:731479176904704060> kullanıcıdetay <@kullanıcı>" , "» Kullanıcı Detaylarını Gösterir!")
-.addField("<a:ayars:731479176904704060> uyar <@kullanıcı> <sebep>" , `» Kullanıcıyı Uyarır!
+.addField("<a:ayars:731479176904704060> uyar <@kullanıcı> <sebep>" , "» Kullanıcıyı Uyarır!")
+.addField("<a:ayars:731479176904704060> uyarılar <@kullanıcı>" , "» Kullanıcının Uyarılarını Gösterir!")
+.addField("<a:ayars:731479176904704060> uyarı-kaldır <@kullanıcı>" , `» Kullanıcının Uyarılarını Kaldırır!
 
 <a:zilbe:731478436731682816> » [Davet Et](Yakında) | [Oyver](Yakında) | [Support Server](https://discord.gg/Z2zuGCR) `)
 message.channel.sendEmbed(mhelp)
