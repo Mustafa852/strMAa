0e6a16148445e8228319072a97e67b3a97fb7bad
Glitch (codework-botu) <none>
2020-07-11 12:02:22 +0000

    🏞🏌 Checkpoint ./komutlar/yardım.js:11195635/276

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,12 +3,12 @@ const Discord = require ("discord.js");
 exports.run = (client, message) => {
 const mhelp = new Discord.RichEmbed()
 .setColor("#5ab1bb")
-.setTitle("Upper Yardım Sistemi")
+.setTitle("Upper Yardım Menüsü")
 .setThumbnail("https://cdn.discordapp.com/attachments/718563633009000531/731477372674768916/unnamed.png")
-.addField("/moderasyon", "Moderasyon Komutlarını Listeler!")
-.addField("/koruma","Koruma Komutlarını Listeler!")
-.addField("/kullanıcı", "Kullanıcı Komutlarını Listeler!")
-.addField("» Linkler",`
+.addField("<a:ayars:731479176904704060> /moderasyon", "Moderasyon Komutlarını Listeler!")
+.addField("<a:ayars:731479176904704060> /koruma","Koruma Komutlarını Listeler!")
+.addField("<a:ayars:731479176904704060> /kullanıcı", "Kullanıcı Komutlarını Listeler!")
+.addField(`<a:zilbe:731478436731682816> Bot Hakkında`,`
       [Davet Et](Yakında) | [Oyver](Yakında) | [Support Server](https://discord.gg/Z2zuGCR) `)
 message.channel.sendEmbed(mhelp)
 
