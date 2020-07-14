5c45c5ac9e3b1a86a5e2dd301739e13e52b12cf6
Glitch (codework-botu) <none>
2020-07-14 20:01:59 +0000

    🐄🌌 Checkpoint ./komutlar/botbilgi.js:11195635/6432 ./komutlar/yardım.js:11195635/338

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,10 +7,10 @@ const mhelp = new Discord.RichEmbed()
 .setThumbnail("https://cdn.discordapp.com/attachments/718563633009000531/731477372674768916/unnamed.png")
 .addField("<a:ayars:731479176904704060> /moderasyon", "» Moderasyon Komutlarını Listeler!")
 .addField("<a:ayars:731479176904704060> /koruma","» Koruma Komutlarını Listeler!")
-.addField("<a:ayars:731479176904704060> /müzik","» Müzik Komutlarını Listeler!")
-.addField("<a:ayars:731479176904704060> /kullanıcı", `» Kullanıcı Komutlarını Listeler!
+.addField("<a:ayars:731479176904704060> /müzik",`» Müzik Komutlarını Listeler!
 
-   <a:zilbe:731478436731682816> » [Davet Et](Yakında) | [Oyver](Yakında) | [Support Server](https://discord.gg/Z2zuGCR) `)
+<a:zilbe:731478436731682816> » [Davet Et](Yakında) | [Oyver](Yakında) | [Support Server](https://discord.gg/Z2zuGCR) `)
+
 message.channel.sendEmbed(mhelp)
 
 }; 
