a319117fae8aba4ae26f590cb351e1742c472aa8
Glitch (codework-botu) <none>
2020-07-14 20:13:52 +0000

    🔦👓 Checkpoint ./komutlar/yardım.js:11195635/819

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,6 +4,7 @@ exports.run = (client, message) => {
 const mhelp = new Discord.RichEmbed()
 .setColor("#5ab1bb")
 .setTitle("Upper Yardım Menüsü")
+.addField("Upper Bot Hakkında!", `Upper Bot Sunucunuzu Daha Kaliteli Hale Getirmek İçin Üretilmiş Bir Bottur. Sizde Bu Bottan Faydanlanmak İçin Aşşağıdaki Linkten Davet Edebilirsiniz! Upper Bot Sadece Yetkili, Koruma Ve Müzik Komutlarını İçermektedir. Herhangi Bir Sorun Oluşursa **/canlıdestek** Yazarak Canlı Destek Ekibimizle Görüşebilirsiniz!\n\n**Sponsor Hakkında!**\nSponsor İsim: Sponsor Aranıyor!\nSponsor Website: Sponsor Aranıyor!\nSponsor Discord: Sponsor Aranıyor!`)
 .setThumbnail("https://cdn.discordapp.com/attachments/718563633009000531/731477372674768916/unnamed.png")
 .addField("<a:ayars:731479176904704060> /moderasyon", "» Moderasyon Komutlarını Listeler!")
 .addField("<a:ayars:731479176904704060> /koruma","» Koruma Komutlarını Listeler!")
