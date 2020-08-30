c26d3eff2d5535a094ed7a0f6df7703b4532c88e
Glitch (upper-bot) <none>
2020-08-30 09:47:03 +0000

    🍛👮 Checkpoint ./komutlar/yardım.js:11195635/3896

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,29 +1,56 @@
-const Discord = require ("discord.js");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json")
+module.exports.run = async (client, message, args) => {
 
-exports.run = (client, message) => {
-const mhelp = new Discord.RichEmbed()
-.setColor("#5ab1bb")
-.setTitle("Upper Yardım Menüsü")
-.setThumbnail("https://cdn.discordapp.com/attachments/718563633009000531/731477372674768916/unnamed.png")
-.addField("<a:ayars:731479176904704060> /moderasyon", "» Moderasyon Komutlarını Listeler!")
-.addField("<a:ayars:731479176904704060> /koruma","» Koruma Komutlarını Listeler!")
-.addField("<a:ayars:731479176904704060> /müzik",`» Müzik Komutlarını Listeler!
+  let sayfalar = [':pencil: **Ana Menü**\nSayfa 2 - Anti Raid Sistemi\nSayfa 3 - Engel ve Tarama Sistemi\nSayfa 4 - Koruma Sistemleri\nSayfa 5 - Yetkili Sistemi'+ client.commands.filter(cmd => exports.help.category === 'admin').map(cmd => '\ ' + exports.help.name + '\ ').join("\n"), '**Yardım Komutları**\n'+client.commands.filter(cmd => exports.help.category === 'util').map(cmd => '\ ' + exports.help.name + '\ ').join("\n"),'**Resim Komutları**\n'+client.commands.filter(cmd => exports.help.category === 'photo').map(cmd => '\ ' + exports.help.name + '\ ').join("\n"),'**Eğlence Komutları**\n'+ client.commands.filter(cmd => exports.help.category === 'fun').map(cmd => '\ ' + exports.help.name + '\ ').join("\n"),'**Müzik**\nmüzik','**NSFW :x:**\n'+ client.commands.filter(cmd => exports.help.category === 'nsfw').map(cmd => '\ ' + exports.help.name + '\ ').join("\n")]; 
+  let sayfa = 1;
 
-<a:zilbe:731478436731682816> » [Davet Et](Yakında) | [Oyver](Yakında) | [Support Server](https://discord.gg/Z2zuGCR) `)
-.setTimestamp()
+  const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setFooter(`Sayfa ${sayfa} - ${sayfalar.length}`)
+    .setDescription(sayfalar[sayfa-1])
 
-message.channel.sendEmbed(mhelp)
+  message.channel.send(embed).then(msg => {
 
-}; 
-exports.conf = {
-    enabled: true, 
-    guildOnly: false, 
-    aliases: ['help'], 
-    permLevel: 0 
-  };
- 
-  exports.help = {
-    name: 'yardım', 
-    description: 'MEE6 HELP MENU',
-    usage: 'help'
-  };
+    msg.react('⏪').then( r => {
+      msg.react('⏩')
+
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id;
+
+      const backwards = msg.createReactionCollector(backwardsFilter);
+      const forwards = msg.createReactionCollector(forwardsFilter);
+
+      backwards.on('collect', r => {
+        if (sayfa === 1) return;
+        sayfa--;
+        embed.setDescription(sayfalar[sayfa-1]);
+        embed.setFooter(`Sayfa ${sayfa} - ${sayfalar.length}`);
+        msg.edit(embed)
+      })
+
+      forwards.on('collect', r => {
+        if (sayfa === sayfalar.length) return;
+        sayfa++;
+        embed.setDescription(sayfalar[sayfa-1]);
+        embed.setFooter(`Sayfa ${sayfa} - ${sayfalar.length}`);
+        msg.edit(embed)
+      })
+
+    })
+
+  })
+}
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["gelişmekteyardım","demoyardım","h","dy"],
+  category: "admin",
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: 'yardım',
+  description: 'Gelişmiş Sayfalı Yardım.',
+  usage: 'yardım'
+};
