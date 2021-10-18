43ec6ddd3371438841612ba9ab8f5a6e45797420
Glitch (mire-clumsy-level) <none>
2021-10-18 15:54:43 +0000

    📍🚶 Checkpoint ./komutlar/yardım rütbe.js:21495835/250 ./komutlar/yardım-s-kur.js:21495835/1169 ./komutlar/yardım-sunucu-tema.js:21495835/1018 ./komutlar/yardım-sunucuKUR.ja:21495835/1187 ./komutlar/yardım.js:21495835/2524 ./komutlar/çokluoynama.js:21495835/2478 ./komutlar/şablongönder.js:21495835/1398 ./komutlar/şikayet.js:21495835/1058 ./event/message.js:21495835/3070

---
+++ komutlar/yardım-sunucu-tema.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const data = require('quick.db');
+
+exports.run = async (client, message, args) => {
+
+
+  
+  
+  message.channel.send(new Discord.MessageEmbed()
+.setColor('#00001')
+.setTitle('Strom')
+.setDescription(`**Bota Oy Vermek için** [TIKLA](https://top.gg/bot/756883309270663229/vote)
+**Bota yorum atıp 5 yıldız vermeyi unutma :)** [TIKLA](https://top.gg/bot/756883309270663229)
+
+**${client.user.username} Sizlere hizmet vermekten şeref duyar..**
+
+İçerikler de birden fazla tema var.
+
+\`s!j4j-tema\` 
+**Hazır __join 4 join__ sunucu teması yollar**
+
+\`s!public-tema\` 
+**Hazır karışık __public__ sunucu teması yollar**
+
+\`s!oyun-tema\` 
+**Hazır __oyun__ sunucu teması yollar**
+
+\`s!gif-tema\` 
+**Hazır gif share sunucu teması yollar**
+
+\`s!sunucu-kur\` 
+**Klasik oyun & sohbet sunucusu kurar**
+
+\`\`\`Birini seç\`\`\``)
+.setThumbnail(''));
+
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['sunucu-tema'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'sunucu-tema-sistem'
+};
