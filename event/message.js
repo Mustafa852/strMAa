c9263e99a753951022c540cff3c77830d73284d2
Glitch (mire-clumsy-level) <none>
2021-10-18 14:09:25 +0000

    🎱🚥 Checkpoint ./Strom.js:21495835/56417 ./ayarlar.json:21495835/620 ./package.json:21495835/2019 ./kasalar.json:21495835/1775 ./kasa9.json:21495835/32 ./kasa8.json:21495835/32 ./kasa7.json:21495835/30 ./kasa6.json:21495835/29 ./kasa5.json:21495835/29 ./kasa4.json:21495835/29 ./kasa3.json:21495835/28 ./kasa2.json:21495835/36 ./kasa15.json:21495835/40 ./kasa14.json:21495835/43 ./kasa13.json:21495835/40 ./kasa12.json:21495835/60 ./kasa11.json:21495835/36 ./kasa10.json:21495835/48 ./kasa1.json:21495835/22 ./sunucutanıt.json:21495835/266 ./util/eventLoader.js:21495835/192 ./event/message.js:21495835/1535 ./event/ready.js:21495835/655 ./komutlar/155 ara.js:21495835/1058 ./komutlar/kayıt otorol.js:21495835/1946 ./database.json:21495835/2 ./config.json:21495835/2 ./captchadata.json:21495835/2 ./Helpers/Database.js:21495835/2514

---
+++ event/message.js
@@ -0,0 +1,49 @@
+const Strom = require("discord.js");
+const ayarlar = require('../ayarlar.json');
+const db = require ("quick.db");
+let talkedRecently = new Set();
+
+module.exports = async ( message, args) => {
+let client = message.client;
+  if (talkedRecently.has(message.author.id)) {
+    return;
+  }
+  talkedRecently.add(message.author.id);
+	setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+  }, 0);
+  if (message.author.bot) return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  
+  
+  
+  
+  
+  
+  else {
+    const laura = [];
+    client.commands.forEach(dropinnem => {
+      laura.push(dropinnem.help.name);
+      dropinnem.conf.aliases.forEach(abcdef => laura.push(abcdef));
+    });
+
+    const rifleman = require('string-similarity').findBestMatch(command, laura);
+    message.channel.send(`bunu mu demek istediniz? **s!${rifleman.bestMatch.target}**`)
+  };
+
+  if (cmd) {
+    if (!message.guild) return
+    if (perms < cmd.conf.permLevel) return;
+    if (db.fetch(`cokaradalistere_${message.author.id}`)) return message.channel.send("Olamaz sen botun karalistesinde bulunuyorsun botu kullanamazsın.")
+    cmd.run(client, message, params, perms);
+  }
+};
---
+++ event/ready.js
@@ -0,0 +1,14 @@
+ const Strom = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+module.exports = async client => {
+  var oyun = ["🔥EN İYİ HİZMET İÇİN BAKIMDAYIZ!🔥"];
+
+  setInterval(async () => {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], { type: "WATCHING" });
+  }, 13000);
+  client.user.setStatus("dnd");
+
+}
