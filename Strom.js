de1ff9efa16bee25fe6d751e37ad8dd08dc03b75
Glitch (mire-clumsy-level) <none>
2021-10-27 16:35:37 +0000

    🎨🏨 Checkpoint ./Strom.js:21495835/1218

--- Strom.js
+++ Strom.js
@@ -620,10 +620,37 @@ client.on("messageDelete", async message => {
 //------------------------ Eklendim Atıldım -----------------------//
 //------------------------ Eklendim Atıldım -----------------------//
 
-// eklendim
-client.on('guildCreate', async guild => { client.channels.get('857903634071027752').send(`${guild}, isimli sunucuya eklendim!`)})
-// atıldım
-client.on('guildRemove', async guild => { client.channels.get('857903634071027752').send(`${guild}, isimli sunucudan atıldım.. :(`)})
+client.on('guildDelete', guild => {
+
+let rrrsembed = new Strom.MessageEmbed()
+
+.setColor("RED")
+.setTitle("Bot Kicklendi")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucu sahibi", guild.owner)
+.addField("Sunucu Sahibi'nin İd'si", guild.ownerID)
+.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.cache.get('857903634071027752').send(rrrsembed);
+  
+});
+
+client.on('guildCreate', guild => {
+
+let rrrsembed = new Strom.MessageEmbed()
+
+.setColor("GREEN")
+.setTitle("Bot Eklendi")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucu sahibi", guild.owner)
+.addField("Sunucu Sahibi'nin İd'si", guild.ownerID)
+.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.cache.get('857903634071027752').send(rrrsembed);
+  
+});
 
 //------------------------ Eklendim Atıldım -----------------------//
 //------------------------ Eklendim Atıldım -----------------------//
